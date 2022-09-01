dbad2b157383216f7573ee751fd64df8470e2e9c
martinwilson001 <martinwilson001@gmail.com>
2022-09-01 23:42:11 +0530

    changes

--- blog-details.php
+++ blog-details.php
@@ -1,5 +1,6 @@
 <?php include('includes/header.php');?>
-<section class="home-page blog-detail-page" style="background-image: url(assets/images/how-can-renting-equipment-be-more-profitable.png);">
+<section class="home-page blog-detail-page"
+    style="background-image: url(assets/images/how-can-renting-equipment-be-more-profitable.png);">
     <div class="national-container">
         <div class="blog-detail-banner">
             <h1>How can renting equipment be more profitable?</h1>
@@ -56,90 +57,131 @@
         </div>
         <div class="contact-address-wrap">
             <div class="rental-main-wrap category-main-wrap">
-                <div class="rental-block">
-                    <img src="assets/images/rental-1.png">
-                    <div class="date-wrap">
-                        <p><span>General</span>21 AUGUST 30 VIEWS</p>
+                <div class="category-detail-wrap">
+                    <p>In 2022 we will continue to see a revolution in the construction industry. Many factors, such as
+                        advancements in technology, rising material costs, eco-friendly and sustainable initiatives,
+                        supply
+                        chain delays,. and labor shortages are forcing many rapid changes in the industry. The
+                        popularity of
+                        utilizing equipment rental is also gaining momentum.</p>
+
+                    <p>National Dispatching offers a one call solution for renting equipment.</p>
+
+                    <h3 class="title">Why is the popularity of equipment rental increasing?</h3>
+
+                    <p>For many reasons, construction equipment rental is becoming more popular. Modern equipment has
+                        revolutionized the way projects are performed and managed. While people are looking at the
+                        budget
+                        and other aspects of the business in search of cost reductions, they can also compare the cost
+                        of
+                        renting or leasing equipment with purchase and ownership costs.</p>
+
+                    <p>One of the biggest challenges for builders is to reduce budget without compromising quality. The
+                        best solution is to find effective labor, equipment, and material resources.</p>
+
+                    <p>In recent years we have seen a major disruption in the financial stability of many private
+                        companies, companies whose investments drive the construction industry. Those companies have
+                        been
+                        looking for ways to reduce their operating costs and as a result are reducing their construction
+                        budgets. At the same time, the cost of production, materials, equipment, and labor are creating
+                        a
+                        ripple effect in the industry.</p>
+
+                    <div class="detail-inner-image-wrap">
+                        <img src="assets/images/blog-inner-1.png">
                     </div>
-                    <div class="category-sub-wrap">
-                        <p>How can renting
-                            equipment be more
-                            profitable?</p>
-                        <small>In 2022 we will continue to see a revolution in the construction industry. Many factors,
-                            such as advancements in technology, rising material costs, eco-friendly and sustainable
-                            initiatives, supply chain delays,. and labor shortages are forcing many rapid changes in the
-                            industry. The popularity of utilizing equipment rental is also gaining momentum.</small>
+
+                    <h3 class="title">Why can renting heavy equipment be better than buying it?</h3>
+
+                    <h4>Helps reduce overall costs</h4>
+                    <p>In the construction industry, a general rule is that if you have a short term need, you should
+                        consider renting it. The purchase of new construction equipment is expensive, if you can find it
+                        these days, and can have a significant impact on an organization’s budget. The rental option has
+                        become popular for many construction companies as it allows them to cut long term costs and run
+                        a
+                        more financially sound business. The quality of the work cannot be compromised at any cost. But
+                        managing costs and productivity is important.</p>
+
+                    <p>In addition, the purchase of equipment is a long-term investment. Initially, the company’s
+                        financial
+                        situation may allow a purchase. But, as the economy changes, so do organizations’ financial
+                        situations. By renting equipment it will help you align your current financial situation with
+                        long-term expectations.</p>
+
+                    <h4>Improves efficiency</h4>
+                    <p>Construction companies are usually juggling multiple projects at once. Different projects may
+                        require different equipment. In those cases, instead of incurring the logistical costs of moving
+                        equipment from project to project, consider renting specific equipment for a specific project’s
+                        locations. This eliminates any delays you may experience and helps to provide each project with
+                        the
+                        proper resources to efficiently complete the project.</p>
+
+                    <h4>It helps to avoid maintenance issues</h4>
+                    <p>Buying and maintaining equipment is not an easy task. The space for storage, regular maintenance
+                        intervals, inspections, etc. should be considered while purchasing equipment along with the
+                        expenses
+                        that come along with it. With renting equipment, the time and labor costs associated with
+                        maintenance and repair are much lower and easier to manage. Instead of worrying about
+                        maintaining
+                        the equipment for its life lifecycle, rentals allow you to focus on the near future instead of
+                        spending time planning for long-term maintenance.</p>
+
+                    <h4>Access to the latest technology</h4>
+                    <p>Once you buy equipment that fits the budget range and requirements of that period, it is not easy
+                        to
+                        upgrade to more advanced equipment at a later time. Technology changes and there is always more
+                        advanced machinery being released to the market. Renting offers you the option to choose from a
+                        wider selection of late model equipment that will allow you to always be using the latest
+                        technology.</p>
+
+                    <div class="detail-inner-image-wrap">
+                        <img src="assets/images/blog-inner-2.png">
                     </div>
+
+                    <h3 class="title">How to choose the right rental partner</h3>
+
+                    <p>We understand that selecting the best equipment supplier for your project is a critical decision
+                        for
+                        any project team. From customer service, to product availability, and price, there is a lot to
+                        consider when choosing the right supplier.</p>
+
+                    <p>At National Dispatching, we focus on building long-lasting relationships with our customers. We
+                        know
+                        that building trust in the services we provide as well as ensuring the quality of products being
+                        supplied is critical to the success of your business. With over 13 years of experience in the
+                        industry, what makes us stand out is our sense of urgency, service, and industry leading
+                        technology.</p>
+
+                    <h4>Access to inventory</h4>
+                    <p>Another factor we are very proud of is our vast supplier network. Every project has different
+                        equipment needs and that’s why having access to the right equipment when you need it is an
+                        essential
+                        part of the equipment supplier relationship. The benefit of renting heavy equipment from
+                        National
+                        Dispatching is that you do not have to worry about availability. We will do whatever it takes to
+                        find the exact equipment you need, when you need it.</p>
+
+                    <h4>Price</h4>
+                    <p>With the current supply chain issues, many rental partners are having a hard time replacing their
+                        aging fleet. At the same time, rental fleet utilization for many categories are at all time
+                        highs.
+                        Prices are following suit. It is our goal to provide you with the best equipment, from the
+                        closest
+                        location, at a competitive price.</p>
+
+                    <h4>Convenience</h4>
+                    <p>At National Dispatching, you can view, select, and book equipment right from our website. Don’s
+                        see
+                        what you need online? That’s not a problem, call us anytime and one of our skilled procurement
+                        coordinators can assist you.</p>
+
+                    <p>National Dispatching can help your company improve productivity and it is our goal to help you
+                        get
+                        your projects completed ahead of time and under budget without compromising quality.</p>
+
                 </div>
-                <div class="rental-block">
-                    <img src="assets/images/rental-2.png">
-                    <div class="date-wrap">
-                        <p><span>General</span>21 AUGUST 30 VIEWS</p>
-                    </div>
-                    <div class="category-sub-wrap">
-                        <p>What to Consider
-                            Before Renting
-                            a Forklift
-                        </p>
-                        <small>Forklifts are used widely throughout many industries. From small material handling units
-                            moving pallets in warehouses to large telehandlers used on construction projects.</small>
-                    </div>
-                </div>
-                <div class="rental-block">
-                    <img src="assets/images/rental-3.png">
-                    <div class="date-wrap">
-                        <p><span>General</span>21 AUGUST 30 VIEWS</p>
-                    </div>
-                    <div class="category-sub-wrap">
-                        <p>What is the Best
-                            Scissor Lift for my
-                            Project?</p>
-                        <small>Scissor Lifts are very efficient at getting personnel, tools, and materials to work areas
-                            that are directly accessible overhead, unlike boom lifts which are typically used in areas
-                            with no direct overhead access.</small>
-                    </div>
-                </div>
-                <div class="rental-block">
-                    <img src="assets/images/rental-4.png">
-                    <div class="date-wrap">
-                        <p><span>General</span>21 AUGUST 30 VIEWS</p>
-                    </div>
-                    <div class="category-sub-wrap">
-                        <p>How can renting
-                            equipment be more
-                            profitable?</p>
-                        <small>In 2022 we will continue to see a revolution in the construction industry. Many factors,
-                            such as advancements in technology, rising material costs, eco-friendly and sustainable
-                            initiatives, supply chain delays,. and labor shortages are forcing many rapid changes in the
-                            industry. The popularity of utilizing equipment rental is also gaining momentum.</small>
-                    </div>
-                </div>
-                <div class="rental-block">
-                    <img src="assets/images/rental-5.png">
-                    <div class="date-wrap">
-                        <p><span>General</span>21 AUGUST 30 VIEWS</p>
-                    </div>
-                    <div class="category-sub-wrap">
-                        <p>What to Consider
-                            Before Renting
-                            a Forklift</p>
-                        <small>Forklifts are used widely throughout many industries. From small material handling units
-                            moving pallets in warehouses to large telehandlers used on construction projects.</small>
-                    </div>
-                </div>
-                <div class="rental-block">
-                    <img src="assets/images/rental-6.png">
-                    <div class="date-wrap">
-                        <p><span>General</span>21 AUGUST 30 VIEWS</p>
-                    </div>
-                    <div class="category-sub-wrap">
-                        <p>What is the Best
-                            Scissor Lift for my
-                            Project?</p>
-                        <small>Scissor Lifts are very efficient at getting personnel, tools, and materials to work areas
-                            that are directly accessible overhead, unlike boom lifts which are typically used in areas
-                            with no direct overhead access.</small>
-                    </div>
+                <div class="share-wrap">
+                    <p>Share This</p><span><img src="assets/images/social-icons.png"></span>
                 </div>
                 <div class="pagination-wrap">
                     <p>Next<span><img src="assets/images/next-icon.png"></span></p>
