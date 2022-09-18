abc74b0e21e49f46be7d3d6837ead94782297c76
martinwilson001 <martinwilson001@gmail.com>
2022-09-18 21:57:45 +0530

    error page

---
+++ error.php
@@ -0,0 +1,37 @@
+<?php include('includes/header.php');?>
+<section class="home-page error-page">
+    <div class="national-container">
+        <h1>Error page 404</h1>
+    </div>
+</section>
+
+<section class="error-main-wrap">
+    <div class="national-container">
+        <h2>The page you are looking for doesn't exist.<br/>
+            Return to the website's homepage to find what you're looking for.</h2>
+        <!-- <div class="btn-wrap"> -->
+            <button class="main-btn"><span>+</span>Send Message</button>
+        <!-- </div> -->
+    </div>
+</section>
+
+
+<!-- Service territory section start -->
+<section class="service-territory">
+    <div class="national-container">
+        <div class="row">
+            <article class="col-lg-12 text-center">
+                <h5 class="ttl-22">Service territory via our qualified partner network</h5>
+                <p>Alabama, Alaska, Arizona,Arkansas, California, Colorado, Connecticut, Delaware, Florida, Georgia,
+                    Hawaii, Idaho, IllinoisIndiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland,
+                    Massachusetts, Michigan, Minnesota, Mississippi, Missouri, MontanaNebraska, Nevada, New
+                    Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma,
+                    Oregon, PennsylvaniaRhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont,
+                    Virginia, Washington, West Virginia, Wisconsin, Wyoming
+                </p>
+            </article>
+        </div>
+    </div>
+</section>
+<!-- Service territory section end -->
+<?php include('includes/footer.php');?>
